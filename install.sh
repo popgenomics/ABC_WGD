16b7c25ce4787ab162d25c3478bb57f00b68598c
popgenomics <camille.roux.1983@gmail.com>
2018-11-30 00:27:26 +0100

    install required tools

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo check the R library abcrf
+wget https://cran.r-project.org/src/contrib/abcrf_1.7.1.tar.gz
+sudo R CMD INSTALL abcrf_1.7.1.tar.gz
+
+echo check msnsam
 if which msnsam >/dev/null; then
 	echo msnsam is ok
 else
@@ -6,6 +11,7 @@ else
 fi
 
 for i in mscalc_wgd.py priorgenwgd run_ABC_polyploid.py; do
+	echo check ${i}
 	if which ${i}>/dev/null; then
 		echo ${i} is ok
 	else
@@ -15,6 +21,7 @@ for i in mscalc_wgd.py priorgenwgd run_ABC_polyploid.py; do
 	fi
 done
 
+echo check pypy
 if which pypy >/dev/null; then
 	echo pypy is ok
 else
@@ -24,7 +31,4 @@ else
 	cd pypy2-v6.0.0-linux32/bin
 	sudo ln -s $PWD/pypy /usr/bin
 fi
-
-wget https://cran.r-project.org/src/contrib/abcrf_1.7.1.tar.gz
-sudo R CMD INSTALL abcrf_1.7.1.tar.gz
 
