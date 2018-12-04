56b988b1699e8bf6da4bdede106cbd3d022ec91a
popgenomics <camille.roux.1983@gmail.com>
2018-12-04 15:09:49 +0100

    ln /usr/local/bin

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ if which msnsam >/dev/null; then
 else
 	echo msnsam has to be installed
 	gcc -O2 -o msnsam  msnsam.c  rand1.c streec.c -lm -w
-	sudo ln -s $PWD/msnsam /usr/bin/
+	sudo ln -s $PWD/msnsam /usr/local/bin/
 fi
 
 for i in mscalc_wgd.py priorgenwgd run_ABC_polyploid.py; do
@@ -19,7 +19,7 @@ for i in mscalc_wgd.py priorgenwgd run_ABC_polyploid.py; do
 	else
 		echo ${i} not in the bin.
 		chmod +x ${i} 
-		sudo ln -s $PWD/${i} /usr/bin/
+		sudo ln -s $PWD/${i} /usr/local/bin/
 	fi
 done
 
@@ -31,6 +31,6 @@ else
 	wget https://bitbucket.org/pypy/pypy/downloads/pypy2-v6.0.0-linux32.tar.bz2
 	pypy2-v6.0.0-linux32.tar.bz2
 	cd pypy2-v6.0.0-linux32/bin
-	sudo ln -s $PWD/pypy /usr/bin
+	sudo ln -s $PWD/pypy /usr/local/bin
 fi
 
